6f17b8cefd68475518bdb73da016abfa843bc505
KonJengz <gecko.jeng@gmail.com>
2023-05-18 00:55:37 +0700

    Update Exercise4_Thaweevit_K.py

---
+++ assignments/Exercise4_Thaweevit_K.py
@@ -0,0 +1,10 @@
+scoreFoundationEng = 70
+scoreGeneralBusiness = 80.6
+scoreIntroComputerSys = 80
+scoreComputerProgram = 70
+
+print('--- Your Score ---')
+print('Foundation English :', scoreFoundationEng, 'คะแนน')
+print('General Business :', scoreGeneralBusiness, 'คะแนน')
+print('Introduction to Computer Systems :', scoreIntroComputerSys, 'คะแนน')
+print('Computer Programming :', scoreComputerProgram, 'คะแนน')
