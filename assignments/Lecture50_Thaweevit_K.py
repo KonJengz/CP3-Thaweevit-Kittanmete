65bfc837f82d1cd1aa266d2dc197155388e048f0
KonJengz <gecko.jeng@gmail.com>
2023-05-25 18:45:17 +0700

    Create Lecture50_Thaweevit_K.py

---
+++ assignments/Lecture50_Thaweevit_K.py
@@ -0,0 +1,13 @@
+x = int(input("x มีค่า = "))
+y = int(input("y มีค่า = "))
+
+def calculate(x, y):
+    print("x =", x)
+    print("y =", y)
+    print("")
+    print("x + y =", x + y)
+    print("x - y =", x - y)
+    print("x * y =", x * y)
+    print("x / y =", x / y)
+
+calculate(x, y)
