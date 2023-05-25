96ef01d79d6eccfd3835f18b5ce632a6b56dc669
KonJengz <gecko.jeng@gmail.com>
2023-05-26 00:04:01 +0700

    Delete Lecture53_Thaweevit_K.py

--- Lecture53_Thaweevit_K.py
+++
@@ -1,6 +0,0 @@
-def vatCalculate(totalPrice):
-    result = totalPrice + (totalPrice*0.07)
-    return result
-
-priceItem = int(input("ราคาสินค้า : "))
-print("ราคารวม VAT =",vatCalculate(priceItem))
