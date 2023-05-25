c6e3fa10ab0d26097b648afd5f8551d7fa205de5
KonJengz <gecko.jeng@gmail.com>
2023-05-25 18:20:24 +0700

    Update Excercise11_Thaweevit_K.py

--- assignments/Excercise11_Thaweevit_K.py
+++ assignments/Excercise11_Thaweevit_K.py
@@ -1,8 +1,4 @@
-num = int(input("Number"))
+num = int(input("Number * : "))
 
 for x in range(num):
-    text = ""
-    for i in range(x+1):
-        textGap = "   " * (num - x)
-        text = (x+(x+1)) * " * "
-    print(textGap,text)
+    print("   " * (num-x), ((x+1)+x) * " * ")
