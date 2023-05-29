28d46012fb2ccbe740da44792730ca24e715eb4d
KonJengz <gecko.jeng@gmail.com>
2023-05-30 06:18:51 +0700

    Create Lecture72_Thaweevit_K.py

---
+++ assignments/Lecture72_Thaweevit_K.py
@@ -0,0 +1,20 @@
+menuList = []
+priceList = []
+while True:
+    menuName = input("ใส่ชื่อเมนู: ")
+    if (menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = int(input("ราคา: "))
+        menuList.append([menuName, menuPrice])
+
+def showBill():
+    totalPrice = 0
+    print("")
+    print("------ menu food ------")
+    for i in range(len(menuList)):
+        print(menuList[i][0], menuList[i][1], "บาท")
+        totalPrice += menuList[i][1]
+    print("ราคารวมทั้งหมด", totalPrice, "บาท")
+
+showBill()
